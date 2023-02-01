95d59acf13856d3a2ed58a87f77635c58e138dc9
karterzhang <karter@126.com>
2023-02-01 17:58:15 +0800

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -4,25 +4,25 @@ pipeline {
     stages {
         stage('Source') {
             steps {
-                echo 'Hello World'
+                echo 'Hello World-Source';hostname
             }
         }
 		
 		stage('Build') {
             steps {
-                echo 'Hello World'
+                echo 'Hello World-Build';pwd
             }
         }
 		
 		stage('Code_Security_Check') {
             steps {
-                echo 'Hello World'
+                echo 'Hello World-Code_Security_Check';date
             }
         }
 		
 		stage('Deploy') {
             steps {
-                echo 'Hello World'
+                echo 'Hello World-Deploy';id
             }
         }
 		
