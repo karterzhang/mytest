aaeed1231242e02a0176a6a8c4b89d5341cecfda
karterzhang <karter@126.com>
2023-02-02 17:27:56 +0800

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -11,7 +11,7 @@ pipeline {
                 parameters {
                     string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
                 }
-            }
+                  }
             steps {
                 echo "Hello, ${PERSON}, nice to meet you."
             }
