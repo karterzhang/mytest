098b706d2a49a001c9590d774d6bf4bab4ee9921
karterzhang <karter@126.com>
2023-02-02 17:31:14 +0800

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -7,14 +7,9 @@ pipeline {
             input {
                 message "Should we continue?"
                 ok "Yes, we should."
-                submitter "alice,bob"
-                parameters {
-                    string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
-                }
+                              
                   }
-            steps {
-                echo "Hello, ${PERSON}, nice to meet you."
-            }
+            
         }
      	    
         stage('Source') {
